9ce6a91fc30d5ed4c31a298f846b04bcca8e482e
Emil <emildavidkov88@gmail.com>
2019-02-21 19:44:07 +0200

    changes

--- script-multiple.js
+++ script-multiple.js
@@ -1,15 +1,28 @@
-var STATUS_URL = "http://localhost:3000/status/";
+var STATUS_URL = "http://35.184.211.77/";
 
-window.onload = function() {
-  for (var i = 0; i < 50; i++) {
+window.onload = initialRequest();
+var container;
+
+function initialRequest() {
+  container = document.getElementById("container");
+  container.innerHTML = "";
+  for (var i = 0; i < 5; i++) {
     getSignleContent(STATUS_URL, renderSquare);
   }
-};
+}
+
+function die() {
+  console.log("DIE");
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", STATUS_URL + "die", true);
+  xhttp.send();
+}
 
 function getSignleContent(url, cb) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+      console.log(xhttp.response);
       var response = JSON.parse(xhttp.response);
       cb(response);
     }
@@ -19,13 +32,9 @@ function getSignleContent(url, cb) {
 }
 
 function renderSquare(status) {
-  var container = document.getElementById("container");
   var color = "background-color: " + status.color;
-  var div = '<div class="box" style="' + color + '">';
-  var span =
-    '<span class="box--content" onclick="getSignleContent(STATUS_URL, renderSquare, ' +
-    status.id +
-    ')"</span>';
+  var div = '<div onclick="die()" class="box" style="' + color + '">';
+  var span = '<span class="box--content"></span>';
   div += span;
   div += "</div>";
   container.innerHTML += div;
